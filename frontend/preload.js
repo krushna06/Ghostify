f3ac0afb822bb5694eafb6599a690572e09430fa
nuu-maan <n82238895@gmail.com>
2025-04-07 03:46:17 +0530

    Fix settings saving and keybind functionality

--- frontend/preload.js
+++ frontend/preload.js
@@ -5,9 +5,24 @@ contextBridge.exposeInMainWorld('electronAPI', {
     updateConfig: (config) => ipcRenderer.invoke('update-config', config),
     toggleWindow: () => ipcRenderer.invoke('toggle-window'),
     getChatHistory: () => ipcRenderer.invoke('get-chat-history'),
-    onUpdateChat: (callback) => ipcRenderer.on('update-chat', callback),
-    onShowChatHistory: (callback) => ipcRenderer.on('show-chat-history', callback),
-    onWindowVisibilityChanged: (callback) => ipcRenderer.on('window-visibility-changed', callback),
-    onScreenshotTaken: (callback) => ipcRenderer.on('screenshot-taken', callback),
-    onOCRProcessing: (callback) => ipcRenderer.on('ocr-processing', callback)
+    onUpdateChat: (callback) => {
+        ipcRenderer.on('update-chat', (_, data) => callback(data));
+        return () => ipcRenderer.removeListener('update-chat', callback);
+    },
+    onShowChatHistory: (callback) => {
+        ipcRenderer.on('show-chat-history', (_, data) => callback(data));
+        return () => ipcRenderer.removeListener('show-chat-history', callback);
+    },
+    onWindowVisibilityChanged: (callback) => {
+        ipcRenderer.on('window-visibility-changed', (_, data) => callback(data));
+        return () => ipcRenderer.removeListener('window-visibility-changed', callback);
+    },
+    onScreenshotTaken: (callback) => {
+        ipcRenderer.on('screenshot-taken', (_, data) => callback(data));
+        return () => ipcRenderer.removeListener('screenshot-taken', callback);
+    },
+    onOCRProcessing: (callback) => {
+        ipcRenderer.on('ocr-processing', (_, data) => callback(data));
+        return () => ipcRenderer.removeListener('ocr-processing', callback);
+    }
 });
